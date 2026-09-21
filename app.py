7bc88d393f0aeb250325b69f4793c1d2d5b8fc3f
Santosh98107 <knlsingh.78@gmail.com>
2026-09-21 15:43:41 +0530

    Refactor VWAP calculation and variable assignments

--- app.py
+++ app.py
@@ -46,13 +46,14 @@ if st.button("Fetch Data"):
  
     volume = data["Volume"]
  
-    vwap = (
-        (data["Close"] * volume).cumsum()
-        / volume.cumsum()
-    )
+vwap = (
+    (data["Close"] * volume).cumsum()
+    / volume.cumsum()
+)
  
-    last_vwap = vwap.iloc[-1].item()
-  close_price = data["Close"].iloc[-1].item()
+last_vwap = vwap.iloc[-1].item()
+ 
+close_price = data["Close"].iloc[-1].item()
  
 data["EMA20"] = data["Close"].ewm(span=20).mean()
 ema20 = data["EMA20"].iloc[-1].item()
