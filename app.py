fc616dc8244c7e568a0bce627c7293b70f5e6e11
Santosh98107 <knlsingh.78@gmail.com>
2026-09-21 16:35:34 +0530

    Refactor metric display layout in app.py

--- app.py
+++ app.py
@@ -61,21 +61,21 @@ ema20 = data["EMA20"].iloc[-1].item()
 data["EMA50"] = data["Close"].ewm(span=50).mean()
 ema50 = data["EMA50"].iloc[-1].item()
  
-    col1, col2 = st.columns(2)
- 
-    with col1:
-        st.metric(
-            "Current Price",
-            round(close_price, 2)
-        )
- 
-        st.metric(
-            "EMA20",
-            round(ema20, 2)
-        )
- 
-    with col2:
-        st.metric(
+   col1, col2 = st.columns(2)
+
+   with col1:
+       st.metric(
+           "Current Price",
+           round(close_price, 2)
+       )
+
+       st.metric(
+           "EMA20",
+           round(ema20, 2)
+       )
+
+   with col2:
+       st.metric(
             "VWAP",
             round(last_vwap, 2)
         )
