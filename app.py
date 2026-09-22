ae7b48a798f0a65672ea45756d097099f9351288
Santosh98107 <knlsingh.78@gmail.com>
2026-09-22 17:59:43 +0530

    Refactor data filling methods in app.py

--- app.py
+++ app.py
@@ -863,8 +863,8 @@ required_cols = [
  
  # data = data.dropna(subset=required_cols).copy()
  
- data = data.fillna(method="bfill")
- data = data.fillna(method="ffill")
+  data = data.fillna(method="bfill")
+  data = data.fillna(method="ffill")
  
 
  
