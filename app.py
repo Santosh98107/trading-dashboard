dd43053718487aec2fccc64084bb39ab59bbfbe5
Santosh98107 <knlsingh.78@gmail.com>
2026-09-25 20:29:58 +0530

    Remove column list display from DataFrame
    Remove unnecessary column display in the DataFrame.

--- app.py
+++ app.py
@@ -824,7 +824,7 @@ def simple_backtest(df):
     
     
     
-    st.write(df.columns.tolist())
+    
     for i in range(30, len(df) - 3):
         sub = df.iloc[:i + 1]
         if len(sub) < 30:
